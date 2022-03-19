1f9999ae39836c34d19c58c8642d2e1260f8e4be
Omilari Mason <omilariamason@gmail.com>
2022-03-19 15:05:10 -0400

    game ID functionality

---
+++ pages/game_search/[gameId].js
@@ -0,0 +1,35 @@
+import {Accordion, Card, Image} from 'react-bootstrap'
+import DealList from '../../components/deal_list';
+
+export async function getServerSideProps({query}){
+    const { gameId } = query;
+    const res = await fetch(`https://www.cheapshark.com/api/1.0/games?id=${gameId}`);
+    const data = await res.json();
+
+    return {
+        props: {
+            data
+        }
+    }
+}
+
+export default function gameSpecifics({data}) {
+    const { info, deals } = data;
+
+    return (
+        <>
+            <Card style={{ width: '25rem'}} inverse>
+                <Card.Img alt={info.title} src={info.thumb} width="100%" />
+                <Card.ImgOverlay>
+                    <Card.Title>{info.title}</Card.Title>
+                    <Card.Subtitle>Steam App Id: {info.steamAppID}</Card.Subtitle>
+                </Card.ImgOverlay>
+            </Card>
+            <DealList deals={deals}/>
+        
+        </>
+    )
+}
+
+
+/*  <DealList deals={deals}/>  */
