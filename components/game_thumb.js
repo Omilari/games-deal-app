6bf8229b0f471e624e16a35d4796f6297efad7f5
Omilari Mason <omilariamason@gmail.com>
2022-03-19 14:57:34 -0400

    updated game_thumb

--- components/game_thumb.js
+++ components/game_thumb.js
@@ -1,14 +1,22 @@
 import { Card } from "react-bootstrap";
+import Link from 'next/link'
 
 const GameList = ({res}) => res.map(game => {
     return(
-        <Card style={{ width: '18rem'}} key={game.gameId} >
-            <Card.Title>{game.external}</Card.Title>
-            <Card.Img variant="top" src={game.thumb} />
-            <Card.Body>"Steam App Id:  {game.steamAppID}"</Card.Body>
-        </Card>
+        <Link href='/game_search/[gameId]' as={`/game_search/${game.gameID}`}>
+            <a>
+                <Card key={game.gameID} style={{width: '18rem'}}>
+                    <Card.Img variant="top" src={game.thumb} />
+                    <Card.Body >
+                        <Card.Title>{game.external}</Card.Title>
+                        <Card.Subtitle>Game Details</Card.Subtitle>
+                        <Card.Text style={{ 'font-size': '13px' }}>Steam App Id:  {game.steamAppID}</Card.Text>
+                        <Card.Text style={{ 'font-size': '13px' }}>Cheapest Deal Id: {game.cheapestDealID}</Card.Text>
+                    </Card.Body>
+                </Card>
+            </a>
+        </Link>
     )
 })
 
 export default GameList;
-
